9f4f0bd3c63f8a74190ceaf4711bf3138d60c713
daisuke-inagaki <chanc.euse.0302@gmail.com>
2019-06-06 16:52:28 +0900

    :sparkles: tasksテーブルの追加

--- db/go_graphql_sample/initdb.d/init.sql
+++ db/go_graphql_sample/initdb.d/init.sql
@@ -3,6 +3,7 @@ CREATE DATABASE sample;
 USE sample;
 
 DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `tasks`;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,          /* ID */
@@ -12,5 +13,17 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,          /* ID */
+  `user_id` int(11) NOT NULL,                    /* USER ID */
+  `title` varchar(255) DEFAULT NULL,             /* 名 */
+  `content` varchar(255) DEFAULT NULL,           /* 姓 */
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fkey_tasks_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
 ;
